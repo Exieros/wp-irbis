632a2ebe97dccab13dc3d99016224fc3e17710e4
Exieros <Exieros@gmail.com>
2022-06-14 16:59:23 +1100

    upd

--- includes/input.js
+++ includes/input.js
@@ -1,51 +1,57 @@
-(function($){
+( function( $ ) {
+	// eslint-disable-next-line camelcase
 	function initialize_field( $el ) {
-		$(':text:visible',$el).autocomplete({
-			source: function( request, response  ){
+		$( ':text:visible', $el ).autocomplete({
+			source: function( request, response  ) {
+				const db = $el.find( 'input' ).data().db ? $el.find( 'input' ).data().db : 'OKIO';
 				let term = request.term.trim();
 
-				if(!term.length)
-					response( [] );
-				
-				$.post( '/wp-json/wp-irbis/v1/execute_query', { 
-					type : '@!!kk1boK_H',
-					db: 'OKIO',
-					'query': 'I=' + term + '$' + ' + B=' + term + '$' + ' + T=' + term + '$'
+				if ( ! term.length ) {
+					response([]);
+				}
+				if ( 'OKIO' === db ) {
+					term += '$';
+				}
+
+				$.post( '/wp-json/wp-irbis/v1/execute_query', {
+					type: '@!!kk1boK_H',
+					db: db,
+					'query': 'I=' + term + ' + B=' + term + ' + T=' + term
 				})
-				.done(function( data ) {
-					let t = data.results.map(e=>{
-						return {
-							value: $(e.description).text(),
-							markdown: e.description.replace('<dd>', '').replace('</dd>', ''),
-							cipher: e.cipher
-						}
-					})
-					response(t)
-				});
+					.done( function( data ) {
+						let t = data.results.map( e=>{
+							return {
+								value: $( e.description ).text(),
+								markdown: e.description.replace( '<dd>', '' ).replace( '</dd>', '' ),
+								cipher: e.cipher
+							};
+						});
+						response( t );
+					});
 			},
 			select: function( event, ui ) {
-				$(this).val(ui.item.cipher)
-				if( !$(this).next().hasClass('biblio') ){
-					$(this).after( "<p class='biblio'>" + ui.item.markdown + "</p>" );
-					$('.biblio').css({
+				$( this ).val( ui.item.cipher );
+				if ( ! $( this ).next().hasClass( 'biblio' ) ) {
+					$( this ).after( '<p class=\'biblio\'>' + ui.item.markdown + '</p>' );
+					$( '.biblio' ).css({
 						fontSize: '1.2em',
 						fontWeight: 400,
 						background: '#faebd7',
 						padding: '20px 50px'
 					});
-				}else{
-					$(this).next().html( ui.item.markdown )
+				} else {
+					$( this ).next().html( ui.item.markdown );
 				}
 				return false;
 			}
 		});
 	}
-	
-	if( typeof acf.add_action !== 'undefined' ) {
-		acf.add_action('ready append', function( $el ){
-			acf.get_fields({ type : 'autocomplete'}, $el).each(function(){		
-				initialize_field( $(this) );
+
+	if ( 'undefined' !== typeof acf.add_action ) {
+		acf.add_action( 'ready append', function( $el ) {
+			acf.get_fields({ type: 'autocomplete'}, $el ).each( function() {
+				initialize_field( $( this ) );
 			});
 		});
 	}
-})(jQuery);
+}( jQuery ) );
